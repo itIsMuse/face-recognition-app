8e57ae6d2e5ef7e4c6e6b604033a3e43710c0e55
itIsMuse <oyeniranshock@gmail.com>
2024-09-22 00:08:51 +0100

    new commit

--- src/components/InputForm/InputText.jsx
+++ src/components/InputForm/InputText.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './InputText.css'
 
+
 const InputText = () => {
 
   const introText = 'The brain will detect faces in your pictures. Give it a try'
   return (
     <>
-    <div className='text-lg w-full border'>
+    <div className='text-lg w-full'>
       <p className='text-center'>{introText}</p>
+      <div className='form p-5 rounded-md shadow-md'>
     <div className='center'>
       <input className='w-3/5'  type="text" />
-      <button className='p-2 bg-yellow-600 w-1/5 transition-transform duration-700 ease-in-out hover:scale-110' type='submit'>Detect</button>
+      <button className='p-1 bg-blue-800 w-1/5 transition-transform duration-700 ease-in-out hover:scale-110' type='submit'>Detect</button>
+    </div>
     </div>
     </div>
     </>

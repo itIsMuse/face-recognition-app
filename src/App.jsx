9642f320b9d9c0ac2f82260dc863b1a88f0ffe9a
itIsMuse <oyeniranshock@gmail.com>
2024-10-03 01:30:23 +0100

    Coded helped and it worked

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import Nav from './components/Nav/Nav'
 import Logo from './components/Logo/Logo'
 import InputText from './components/InputForm/InputText'
 import FaceRecognition from './components/FaceRecognition'
-import Rank from './components/Rank'
+import './App.css'
+import ParticlesComponent from './components/particles'
+
 const App = () => {
 
+
   return (
-    <>
+    
       <div className='App'>
+<ParticlesComponent id = 'particles'/>
         <Nav />
         <Logo />
         <Rank />
          <InputText />
         {/* <FaceRecognition /> */}
       </div>
-    </>
   )
 }
 

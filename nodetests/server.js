77d23a3b3fcae1cc2a9d77b5c7d16c9cc85fd8fb
itIsMuse <oyeniranshock@gmail.com>
2024-11-18 06:55:19 +0100

    still working on backend , i am on postman

--- nodetests/server.js
+++ nodetests/server.js
@@ -1,5 +1,26 @@
 import express from 'express'
 
 const app = express()
+// const user = {
+//     'name' : 'Museya',
+//     'attitude': 'chill'
+// }
+
+    app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('helooo')
+})
+
+app.get('/profile', (req, res) => {
+console.log(req.query)
+res.send('hello, world')
+})
+
+app.post('/profile', (req, res) => {
+    console.log(req.body)
+    res.send('sucess')
+    })
 
 app.listen(4000)

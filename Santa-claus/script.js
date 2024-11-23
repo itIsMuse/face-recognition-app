a1da009dc3679dd8c13537e9a7daa61fa1874bc6
itIsMuse <oyeniranshock@gmail.com>
2024-11-23 06:48:28 +0100

    still on the matter

--- Santa-claus/script.js
+++ Santa-claus/script.js
@@ -1,23 +1,18 @@
 import fs from 'fs'
 
-const rawFile = fs.readFileSync('./hello.txt')
+function question1(){
+  fs.readFile('./hello.txt', (err, data) => {
+    const direction = data.toString()
+    const directionToArray = direction.split('')
+    const answer = directionToArray.reduce((acc, symbol) => {
+      if(symbol === '('){
+        return acc += 1
+      } else if (symbol === ')'){
+        return acc -= 1
+      } return acc
+    }, 0)
+    console.log(answer)
+  })
+}
 
-const stringed = rawFile.toString()
-
-const arrayed = stringed.split('')
-
-const counter = arrayed.reduce((acc, symbol, index) => {
-  if(symbol === '('){
-    symbol = '40'
-    acc += 1
-
-  } else if (symbol === ')'){
-    symbol = '41'
-    acc -= 1
-    
-  }
-  console.log(`Step ${index}: Symbol: ${symbol}, Accumulator: ${acc}`);
-  return acc
-}, 0)
-
-console.log(counter)
+question1()

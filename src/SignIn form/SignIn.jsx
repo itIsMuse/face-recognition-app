4fc5b5135523be9422504f9187b83f241cdf3511
itIsMuse <oyeniranshock@gmail.com>
2024-12-01 06:22:01 +0100

    trying to understand the logic of the sign in page and mark attendance

--- src/SignIn form/SignIn.jsx
+++ src/SignIn form/SignIn.jsx
@@ -7,15 +7,16 @@ const SignIn = ({onRouteChange, onAuthenticate}) => {
   const navigate = useNavigate(); // For programmatic navigation
 
   const handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
+    fetch('http://localhost:4500/signin', () => {
 
     // Simulate a successful login check (replace with real auth logic)
-    if (email === "user@example.com" && password === "password123") {
+    if (email === 'databaseemail' && password === "password123") {
       onAuthenticate(); // Call the parent function to set authenticated state
       navigate("/home"); // Redirect to home after successful sign-in
     } else {
       alert("Invalid login credentials");
-    }
+    }})
   };
   return (
 <form className="max-w-sm mx-auto shadow-xl">
@@ -29,10 +30,11 @@ const SignIn = ({onRouteChange, onAuthenticate}) => {
     <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
   </div>
  
-  <button onClick={onAuthenticate} type="submit" className=" mb-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+  <button onClick={handleSubmit} type="submit" className=" mb-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
 </div>
 </form>
   );
 };
 
+// i just used this one to mark attendance oooo, i am not sure it work i just want it to show that i contributed something on monday
 export default SignIn;

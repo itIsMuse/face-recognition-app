f098f5c516d828264d7884351b5c9b7909c3997f
itIsMuse <oyeniranshock@gmail.com>
2025-01-01 07:48:38 +0100

    changed home to the original source

--- src/App.jsx
+++ src/App.jsx
@@ -171,7 +171,7 @@ const handleSignInOff = () => {
         <Nav removeAuthentication={removeAuthentication} signedIn ={signedIn} handleSignInOff ={handleSignInOff} />
 
         <Routes>
-          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="/" element={<Navigate to="https://facerecognitionfrommuseya.netlify.app" />} />
           <Route path="/signin" element={<SignIn onAuthenticate={handleAuthentication} signedIn = {handleSignInOn}  loadUser = {loadUser} />} />
           <Route path="/register" element={<Register handleRegister={handleRegister} onAuthenticate = {handleAuthentication} signedIn = {handleSignInOn}/>} />
           <Route

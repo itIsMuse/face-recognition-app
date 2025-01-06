af56fe1344face28cb45d0516693dfebf40cdd4e
itIsMuse <oyeniranshock@gmail.com>
2025-01-06 05:34:15 +0100

    minifield error solving

--- src/SignIn form/SignIn.jsx
+++ src/SignIn form/SignIn.jsx
@@ -60,7 +60,7 @@ const SignIn = ({signedIn, onAuthenticate, loadUser}) => {
   
   return (
     <div>
-      {}
+      
         {loading && (
           <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
             <RingLoader color="#36d7b7" size={100} />

17fbc71b147952d987cfcfa328f51c9cfff6f2ba
itIsMuse <oyeniranshock@gmail.com>
2025-01-06 05:25:54 +0100

    ring loader imported

--- src/SignIn form/SignIn.jsx
+++ src/SignIn form/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { RingLoader } from 'react-spinners/RingLoader';
 
 const SignIn = ({signedIn, onAuthenticate, loadUser}) => {
   const [email, setEmail] = useState("");
